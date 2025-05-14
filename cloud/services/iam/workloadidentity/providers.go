fa2a871b7315da453270edf990d63f8a02ea6247
Matt Morrison <matt.morrison@ditto.live>
2025-05-15 08:39:12 +1200

    Get project number to build WI provider parent

--- cloud/services/iam/workloadidentity/providers.go
+++ cloud/services/iam/workloadidentity/providers.go
@@ -14,7 +14,8 @@ type workloadIdentityPoolProvidersInterface interface {
 }
 
 func (s *Service) InsertProvider(_ context.Context, spec *iam.WorkloadIdentityPoolProvider) error {
-	_, err := s.widpp.Create(fmt.Sprintf("%s/workloadIdentityPools/%s", s.parent(), spec.Name), spec).WorkloadIdentityPoolProviderId(spec.Name).Do()
+	poolId := fmt.Sprintf("%s/workloadIdentityPools/%s", s.parentByProjectNumber(), s.scope.WorkloadIdentityPoolSpec().Name)
+	_, err := s.widpp.Create(poolId, spec).WorkloadIdentityPoolProviderId(spec.Name).Do()
 	if err != nil {
 		return fmt.Errorf("failed to create workload identity pool provider: %w", err)
 	}
@@ -24,7 +25,7 @@ func (s *Service) InsertProvider(_ context.Context, spec *iam.WorkloadIdentityPo
 
 // GetProvider implements workloadIdentityPoolProvidersInterface.
 func (s *Service) GetProvider(ctx context.Context, id string) (*iam.WorkloadIdentityPoolProvider, error) {
-	provider, err := s.widpp.Get(fmt.Sprintf("%s/workloadIdentityPools/%s/providers/%s", s.parent(), s.scope.WorkloadIdentityPoolSpec().Name, id)).Do()
+	provider, err := s.widpp.Get(fmt.Sprintf("%s/workloadIdentityPools/%s/providers/%s", s.parentByProjectNumber(), s.scope.WorkloadIdentityPoolSpec().Name, id)).Do()
 	if err != nil {
 		return nil, err
 	}
@@ -34,7 +35,7 @@ func (s *Service) GetProvider(ctx context.Context, id string) (*iam.WorkloadIden
 
 // DeleteProvider implements workloadIdentityPoolProvidersInterface.
 func (s *Service) DeleteProvider(ctx context.Context, id string) error {
-	_, err := s.widpp.Delete(fmt.Sprintf("%s/workloadIdentityPools/%s/providers/%s", s.parent(), s.scope.WorkloadIdentityPoolSpec().Name, id)).Do()
+	_, err := s.widpp.Delete(fmt.Sprintf("%s/workloadIdentityPools/%s/providers/%s", s.parentByProjectNumber(), s.scope.WorkloadIdentityPoolSpec().Name, id)).Do()
 	if err != nil {
 		return fmt.Errorf("failed to delete workload identity pool provider: %w", err)
 	}
